7c44fac4e308e1aee168921afb7ee8269e20383a
bohodays <bohodays0316@gmail.com>
2022-12-24 00:13:15 +0900

    Update: 06_hooks

---
+++ 06_hooks/hook-test-app/src/main.tsx
@@ -0,0 +1,16 @@
+import React from 'react'
+import ReactDOM from 'react-dom/client'
+// import App from './App01'
+// import App from './App02'
+// import App from './App03'
+// import App from './App04'
+// import App from './App05'
+// import App from './App06'
+import App from './App07'
+import './index.css'
+
+ReactDOM.createRoot(document.getElementById('root') as HTMLElement).render(
+  <React.StrictMode>
+    <App />
+  </React.StrictMode>,
+)
